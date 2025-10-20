8f874c338fcb643c7366cf8700babd9091762ee7
binhhuynguyen22-hub <binhhuynguyen22@gmail.com>
2025-10-20 10:00:56 +0700

    Add files via upload

--- bt5ss3.c
+++ bt5ss3.c
@@ -6,25 +6,25 @@ int main()
 	printf("-------------------------------------------------------------------------------\n");
 	printf("|STT | Ho va ten          |  Tuoi | So dien thoai |  email                      |\n");
 	printf("-------------------------------------------------------------------------------\n");
-	printf("| 1  | Nguyen Van A       | 20    | 0904488481    | anv@rikkeiacademy.com       |\n");
+	printf("|%-4d|%-20s|%-7d|%-15s|%-29s|\n",1,"Nguyen Van A",20,"0904448481","anv@rikkeiacademy.com");
 	printf("-------------------------------------------------------------------------------\n");
-	printf("| 2  | Nguyen Van B       | 21    | 0904488482    | bnv@rikkeiacademy.com       |\n");
+	printf("|%-4d|%-20s|%-7d|%-15s|%-29s|\n",2,"Nguyen Van B",21,"0904448482","bnv@rikkeiacademy.com");
 	printf("-------------------------------------------------------------------------------\n");
-	printf("| 3  | Nguyen Van C       | 18    | 0904488483    | cnv@rikkeiacademy.com       |\n");
+	printf("|%-4d|%-20s|%-7d|%-15s|%-29s|\n",3,"Nguyen Van C",18,"0904448483","cnv@rikkeiacademy.com");
 	printf("-------------------------------------------------------------------------------\n");
-	printf("| 4  | Nguyen Van D       | 19    | 0904488484    | dnv@rikkeiacademy.com       |\n");
+	printf("|%-4d|%-20s|%-7d|%-15s|%-29s|\n",4,"Nguyen Van D",19,"0904448484","dnv@rikkeiacademy.com");
 	printf("-------------------------------------------------------------------------------\n");
-	printf("| 5  | Nguyen Van E       | 22    | 0904488485    | env@rikkeiacademy.com       |\n");
+	printf("|%-4d|%-20s|%-7d|%-15s|%-29s|\n",5,"Nguyen Van E",22,"0904448485","env@rikkeiacademy.com");
 	printf("-------------------------------------------------------------------------------\n");
-	printf("| 6  | Nguyen Van F       | 21    | 0904488486    | fnv@rikkeiacademy.com       |\n");
+	printf("|%-4d|%-20s|%-7d|%-15s|%-29s|\n",6,"Nguyen Van F",21,"0904448486","fnv@rikkeiacademy.com");
 	printf("-------------------------------------------------------------------------------\n");
-	printf("| 7  | Nguyen Van G       | 23    | 0904488487    | gnv@rikkeiacademy.com       |\n");
+	printf("|%-4d|%-20s|%-7d|%-15s|%-29s|\n",7,"Nguyen Van G",23,"0904448487","gnv@rikkeiacademy.com");
 	printf("-------------------------------------------------------------------------------\n");
-	printf("| 8  | Nguyen Van H       | 19    | 0904488488    | hnv@rikkeiacademy.com       |\n");
+	printf("|%-4d|%-20s|%-7d|%-15s|%-29s|\n",8,"Nguyen Van H",19,"0904448488","hnv@rikkeiacademy.com");
 	printf("-------------------------------------------------------------------------------\n");
-	printf("| 9  | Nguyen Van I       | 18    | 0904488489    | inv@rikkeiacademy.com       |\n");
+	printf("|%-4d|%-20s|%-7d|%-15s|%-29s|\n",9,"Nguyen Van I",18,"0904448489","inv@rikkeiacademy.com");
 	printf("-------------------------------------------------------------------------------\n");
-	printf("| 10 | Nguyen Van K       | 21    | 0904488480    | knv@rikkeiacademy.com       |\n");
-	printf("-------------------------------------------------------------------------------\n");
+	printf("|%-4d|%-20s|%-7d|%-15s|%-29s|\n",10,"Nguyen Van K",21,"09044484810","knv@rikkeiacademy.com");
+	
 }
 
